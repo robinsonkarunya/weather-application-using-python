dc3877a3a0b3b5af9cefdec975cafd605ec4b6ca
Robinson P <robinsonp@karunya.edu>
2025-09-11 12:19:06 +0530

    first commit

---
+++ server/server.py
@@ -0,0 +1,8 @@
+import http.server
+import socketserver
+
+PORT = 8000
+Handler =  http.server.SimpleHTTPRequestHandler
+with socketserver.TCPServer((("",PORT)),Handler) as httpd:
+    print(f"serving http localhost: {PORT}")
+    httpd.serve_forever()
